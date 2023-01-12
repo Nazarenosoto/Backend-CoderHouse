c45faaa49fd4ec4b028f157ab882b9db7407838a
NazarenoWeb <nazitasoto99@gmail.com>
2023-01-12 09:17:26 -0300

    corregí lo visto

--- index.js
+++ index.js
@@ -1,29 +1,51 @@
-//se tiene que hacer una clase para agregar productos y traer productos, lo mismo que se hizo en la clase pero mas renderizado
 class producManager{
     constructor() {
-        this.getProducts = []
+        this.products = []
     }
-    getProduct = () => this.getProducts
-    addProduct = (title,description,price,thumbnail,code,stock) => {
-        const getProductById = {
-            title,
-            description,
-            price,
-            thumbnail,
-            code,
-            stock
+    getProduct = () => this.products
+    getProductById = (id) => {
+        const productDb = this.products.find(productos => productos.id === id)
+        if(!productDb){
+            return 'El producto no existe'
         }
-        if (this.getProducts.length === 0) {
-            getProductById.id = 1
-        } else {
-            return console.log('Not found')
-        }
-        this.getProducts.push(getProductById)
-        console.log(this.getProducts)
+        return productDb
     }
-    
+
+    addProduct = (newItem) => {
+        const productDb = this.products.find(productos => productos.code === newItem.code)
+        if (productDb){
+            return 'Se encuentra el producto'
+        }
+        //Validar el campo
+        if(newItem.title === ''){ return 'Llenar los campos!'}
+
+        if (this.products.length === 0){
+            newItem.id = 1
+            this.products.push(newItem)
+        }else{
+            this.products = [... this.products, {... newItem, id: this.products[this.products.length - 1].id + 1 } ]
+        }
+
+    }
 }
-const productManager = new producManager()
-productManager.addProduct('producto prueba', 'este es un producto de prueba', 200, 'sin imagen','abc123',25)
 
+const Products =  new producManager()
 
+console.log(Products.addProduct({
+    title: '',
+    description: 'Esto es un producto',
+    price: 250,
+    thumbnail: 'ruta image',
+    code: 1,
+    stock: 20
+}))
+console.log(Products.addProduct({
+    title: 'Este si tiene titulo',
+    description: 'Esto es un producto',
+    price: 250,
+    thumbnail: 'ruta image',
+    code: 2,
+    stock: 20
+}))
+
+console.log(Products.getProductById(2))
